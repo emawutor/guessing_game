5bdef3b9be021d46308d82dbc63f4ae353b74c14
emmanuel mawutor <emawutor@indiana.edu>
2018-09-13 22:00:55 -0700

    ore push notes

--- src/chapter_notes.rs
+++ src/chapter_notes.rs
@@ -85,7 +85,7 @@
           // array.iter().enumerate() enumerate returns and index and the element
           // for (i = index, &item= references the elements within an array)
           // &s reference a word and  [0..range] defines how much you want from that word; &s[0..range]
-          // &s ..= means we are including the last number
+          // &s ..= means we are including the last number or ... inclusive .. not invlusive
           // byte is an element in an array
 
 
@@ -105,9 +105,15 @@
           // structs are accessed with dot notation
           // structs are private by default but can be made public using (pub)
           // modules are name spaced with structs that are defined within
-          // updating structs you, reassing the values
+          // updating structs you, reassign the values wanted to change but what you dont change you can use ..x to copy
+          // struct can be instantiated with out a value
         // Methods syntax
           // impl used define a method within an object
+            // can create method for structs and enums;
+          // Self: determines what kind of ownership in a method
+            // &self: the method borrows the value: commonly used
+            // &mut self: method borrows an modifies struct
+            // self : takes ownership
           // rare that a method takes ownership of an object
           // . to call methos on a object: rust automatically adds in &, &mut, or * on object when you call a method
           // rust makes borrowing implicit in methods
@@ -115,14 +121,93 @@
           // using impl(implementation) : allows definining functions within an object without using it as a method
 
   // Chapter 6: Enums and Pattern Matching**
+        // enum is a way of expressing some data that may be one of several things (datatypes)
+        // structs contain one data type: struct contains all asians and an enum contains black, white, asians. Objects with similarity vs Objects unique to themselves
         // variants of enum are namespaced under its identifier ::
-        // enums allows one to store 4 different
+        // enums allows one to store different datatypes
         // you can pass any type of parameters within an enum; enum variable_name { variant_Name()}
         // enums are several structs within a function
         // enums just like structs have methods
+        // Structs & enums are stored inline by default, so they may not be recursive
+          // inline meaning elements are stored a called on every where they are called so recursive calls must be specified or you have an infinte size at compile time
+          // box is a term for allocating data on a heap
+          //Box<variable_name> is a heap pointer with exactly one owner
+          //
         // option
          // rust doesnt have null value
-         //
+         // use of None Some(T)
+         // option<T> means a value may or may not exit: wild card
+         // helpful for objects that can be created with missing values. option<T> doesnt give you null but rather allows you to work without input
+         // every option must be accompanied with a Some(i) and a None incase some does not exist or a placeholder
+        // Match
+          // ref used in match statements to reference a variable
+          // if-let
+          // while-let : keeps running until condition fails to match
+          // binding() passing enums that are binding to other enums or objects
+          // _ Placeholder
+            // _ placeholder ignores all tools before
+        // Concise Control Flow with if let
+          // if let is a more concise match statement
+            // if let statement = expression {}
+            // sugar syntax for match
+
+  // Chapter 7: Modules
+        // mod declares a new module
+        // cargo new project_name --lib
+        // cargo build used to compile crates : equal to bundle in rails
+        // cargo run == rake
+        // module are good for grouping
+        // *difference between library crate and binary crate*
+        // modules call modules that are already defined with functions
+        // use allows you to extract values from a mod with out explicitly naming each value
+        // each module has its own folder
+          // module -> module.rs (all functions go in there) -> submoduel(name of the module as a file -> module.rs)
+          // lib.rs calls the main modules
+            // mod A; and mod B;
+              // mod A does not have a sub file: the file will just be A.rs
+              // mod B has sub files so the foler will have a mod.rs file with a function and call mod C a file within mod B.rs
+              // C.rs will contain the functions that are attached to it: inheritance with namespace
+        // Public vs Private
+          // default state in all rust code is private
+          // use pub to make modules public
+            // use allows a shortened version of namespacing
+            // use TrafficLight::{Red, Yellow}; enums
+            // * to bring all into scope * global scope
+            // super used to move up one module in a hierarchy
+            //
+
+  // Chapter 8: Common Collections
+    // collections are stored on a heap
+    // Vectors: Vectors can only store values of the same type
+      // usefule to for list items: text file or prices of items in a shopping cart
+      // heap is good for when you have data that has many occurence then the pointer can take you to the group that sopecific to a data
+      // v: datatype Vec<i32> = Vec::new()
+      // updating vec: v.push(value)
+      // when you leave the scope of vectors, they are droped
+      // you can use either index number or .get method to access values in an index
+      //  use index &v[] method if want the program to crash if user access goes out of range but if its common that the vector usually goes out of range, you can use .get
+      // since vectors require memory; retrieving a value and updating will not work at once
+      // use for look to itterate through a vector
+      // use * operate to get the value in a vector in order to change
+      // in order to use a vector with different data type; you store enum in the vectore
+      // vec! used to instantiate a new vector
+    // Strings
+      // .to_string used to create a string
+      // String::from(literal) : used to create strings from literals
+      //push_str : used to update a string, add string to a string
+      // use .push to push characters to a string
+      // format!({}{}{}, match) used for more complex concatnation
+      // strings are saved on a heap as a UTF8 code so accessing each char is impossible because they arent saved by the index number
+      // use slicing with a range to define what you want to extract from a data set
+      // first turn the string into array using .char .bytes: return raw bytes
+    // Hash Maps
+      //
+
+  // Chapter 10: Generic Types, Traits, and Lifetimes
+      // <T> to instantiate a genric; you pass that generic value to your function or variable type with what data type that coincide with the function or variable
+
+
+
 
 
 
